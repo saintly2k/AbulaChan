32a5d8c43c396cec2b05477fcdfe3eec4fa18c62
$aintly <63718132+saintly2k@users.noreply.github.com>
2021-05-31 20:54:49 +0200

    config template

---
+++ templates/config.php
@@ -0,0 +1,33 @@
+<?php
+
+//SaintBoard - The worst Textboard/Imageboard out there
+
+//header
+
+//you need to change these thingies!
+
+$mypagetitle = 'AbulaBoard';
+// what's the name of the board?
+$subtitle = '/H/entai';
+//your subtitle lol
+$antispam_word = 'hanime';
+//eg: "nospam", used to prevent spam
+$mysecretword = 'qwertz';
+//this is just used for some crayshit. just remember it.
+
+//image functions (yes, but it is GLITCHY! i recommend false)
+
+$enable_guest_images = false; //can people post using images?
+//true or false (false is recomendet & default)
+$default_guest_image = "http://abulafia.space/haha/43E34958-3166-42D9-BCB1-F4EB7D20089A.jpeg"; //this pic is shown if a user doesn't enter a url to a pic
+$guest_image_size = 40;
+// doesn't work yet, that's why i recommend turning this function off
+
+//other customization
+
+$page_comments = 10;
+// number of texts per page (TEN IS RECOMENDED)
+$time_zone = 0;
+//GMT = 0 while for example, EST = GMT-5, so replace 0 with -5
+
+?>
