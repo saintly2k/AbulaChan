fe527d556dd318dc28caf03ec7c142f3a3340f55
$aintly <63718132+saintly2k@users.noreply.github.com>
2021-05-31 20:58:19 +0200

    haha v2

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 
-$pageheader = '[ <a href="../a/">a</a> / <a href="../m/">m</a> / <a href="../n/">n</a> / <a href="../h/">h</a> ] [ <a href="../rules/">rules</a> / <a href="../about/">about</a> / <a href="../">home</a>]';
+$pageheader = '[ <a href="template/">template</a></center> ] [ <a href=""> home</a> ]';
 
 ?>
 
