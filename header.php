6e24a7f0008a4957d342a1074591d6c65e1bf62b
$aintly <63718132+saintly2k@users.noreply.github.com>
2021-05-31 20:46:18 +0200

    yeye

---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+
+$pageheader = '[ <a href="../a/">a</a> / <a href="../m/">m</a> / <a href="../n/">n</a> / <a href="../h/">h</a> ] [ <a href="../rules/">rules</a> / <a href="../about/">about</a> / <a href="../">home</a>]';
+
+?>
+
+<!-- if you have other boards, you need to repalce this!!! -->
