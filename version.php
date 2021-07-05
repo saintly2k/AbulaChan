40ce2498d24804ca4fdc887cba7035f5ed1c17f4
Moonstone <sejo.gaming.com@gmail.com>
2021-07-05 17:33:28 +0200

    v0.2.2 yesyes

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
-$version = '(Running on AbulaChan v0.2 <a href="https://github.com/saintly2k/AbulaChan" target="_BLANK">Download</a>)';
+$version = '(Running on AbulaChan v0.2.2 <a href="https://github.com/saintly2k/AbulaChan" target="_BLANK">Download</a>)';
 
 ?>
